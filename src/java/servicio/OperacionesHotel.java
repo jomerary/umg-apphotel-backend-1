1159e274cc4b499ee29dd5a94fe6d2f479b41f66
Andre Ortega <andre.ortega.jg@gmail.com>
2025-10-18 00:31:16 -0600

    test hotel

--- src/java/servicio/OperacionesHotel.java
+++ src/java/servicio/OperacionesHotel.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.DELETE;
-
+import jakarta.ws.rs.core.Response;
 
 /**
  * REST Web Service
@@ -29,6 +29,7 @@ import jakarta.ws.rs.DELETE;
  */
 @Path("hotel")
 public class OperacionesHotel {
+
     Conexion cn = new Conexion();
     Connection con;
     PreparedStatement ps;
@@ -36,9 +37,10 @@ public class OperacionesHotel {
 
     @Context
     private UriInfo context;
+
     public List<Hotel> Consultar() {
         List<Hotel> lista = new ArrayList<>();
-        String sql = "SELECT * FROM hotel";
+        String sql = "select * from hotel";
 
         try {
             con = cn.getConnection();
@@ -48,14 +50,13 @@ public class OperacionesHotel {
             while (rs.next()) {
                 Hotel u = new Hotel();
                 u.setHotelId(rs.getInt("hotel_id")); // si tu clase tiene este campo
-                u.setNombre(rs.getString("nombre_usuario"));
+                u.setNombre(rs.getString("nombre"));
                 u.setDireccion(rs.getString("direccion"));
                 u.setTelefono(rs.getString("telefono"));
                 u.setCorreo(rs.getString("correo"));
                 u.setFechaRegistro(rs.getTimestamp("fecha_registro").toLocalDateTime());
-                
+
                 // Te amo mi vida hermosa <3
-                
                 //  aqui se encuentra el error, para que la lista muestre algo tiene que agregarle algo
                 lista.add(u); // <----- linea faltante
             }
@@ -79,8 +80,8 @@ public class OperacionesHotel {
     @Path("/agregar")
     @Produces("application/json")
     @Consumes("application/json")
-    public int agregar(Hotel u) {
-        String sql = "insert into Hotel(hotel_id,nombre,direccion,telefono,correo) values (?,?,?,?,?)";
+    public Response agregar(Hotel u) {
+        String sql = "insert into hotel(hotel_id,nombre,direccion,telefono,correo) values (?,?,?,?,?)";
         try {
             con = cn.getConnection();
             ps = con.prepareStatement(sql);
@@ -91,44 +92,56 @@ public class OperacionesHotel {
             ps.setString(4, u.getTelefono());
             ps.setString(5, u.getCorreo());
             ps.executeUpdate();
+            int rows = ps.executeUpdate();
+
+            if (rows > 0) {
+                return Response.ok()
+                        .entity("{\"message\": \"Hotel creado correctamente\"}")
+                        .build();
+            } else {
+                return Response.status(Response.Status.NOT_FOUND)
+                        .entity("{\"error\": \"Algo Ocurrio\"}")
+                        .build();
+            }
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("{\"error\": \"" + e.getMessage().replace("\"", "\\\"") + "\"}")
+                    .build();
+        }
+
+    }
+
+    @PUT
+    @Path("/modificar")
+
+    public int modificar(Hotel u) {
+        String sql = "update hotel set nombre=?, direccion=?, telefono=?, correo=? where hotel_id=?";
+        try {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setInt(5, u.getHotelId());
+            ps.setString(1, u.getNombre());
+            ps.setString(2, u.getDireccion());
+            ps.setString(3, u.getTelefono());
+            ps.setString(4, u.getCorreo());
+            ps.executeUpdate();
             return 1;
         } catch (Exception e) {
             return 0;
         }
+    }
 
-    }
-    @PUT
-    @Path("/modificar")
-    
-    public int modificar(Hotel u){
-        String sql= "update hotel set nombre=?, direccion=?, telefono=?, correo=? where hotel_id=?";
-        try
-        {
-        con = cn.getConnection();
-        ps=con.prepareStatement(sql);
-        ps.setInt(5, u.getHotelId());
-        ps.setString(1, u.getNombre());
-        ps.setString(2, u.getDireccion());
-        ps.setString(3, u.getTelefono());
-        ps.setString(4, u.getCorreo());
-        ps.executeUpdate();
-        return 1;
-        }catch(Exception e){
-            return 0;
-        }
-    }
     @GET
     @Path("/consultar/{id}")
-    public List<Hotel> consultar (@PathParam("id") int Hotel_id){
+    public List<Hotel> consultar(@PathParam("id") int Hotel_id) {
         List<Hotel> lista = new ArrayList<>();
-        String sql="select * from hotel where hotel_id=?";
-        try{
+        String sql = "select * from hotel where hotel_id=?";
+        try {
             con = cn.getConnection();
             ps = con.prepareStatement(sql);
             ps.setInt(1, Hotel_id);
-            rs=ps.executeQuery();
-            while(rs.next())
-            {
+            rs = ps.executeQuery();
+            while (rs.next()) {
                 Hotel u = new Hotel();
                 u.setHotelId(rs.getInt("hotel_id"));
                 u.setNombre(rs.getString("nombre"));
@@ -137,33 +150,32 @@ public class OperacionesHotel {
                 u.setCorreo(rs.getString("correo"));
                 lista.add(u);
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return lista;
-        
+
     }
+
     @DELETE
     @Path("/eliminar/{id}")
-    public int eliminar(@PathParam("id") int hotel_id){
-        String sql="delete from hotel where hotel_id=?";
-        try{
-            con =cn.getConnection();
-            ps=con.prepareStatement(sql);
+    public int eliminar(@PathParam("id") int hotel_id) {
+        String sql = "delete from hotel where hotel_id=?";
+        try {
+            con = cn.getConnection();
+            ps = con.prepareStatement(sql);
             ps.setInt(1, hotel_id);
             ps.executeUpdate();
             return 1;
-        }catch(Exception e)
-        {
+        } catch (Exception e) {
             return 0;
         }
     }
+
     /**
      * Creates a new instance of OperacionesHotel
      */
     public OperacionesHotel() {
     }
 
-    
-   
 }
